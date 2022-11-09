f89aaf2e4e891b313e589279ba5dc3f6a936e179
alex1151212 <alex1151212@gmail.com>
2022-11-10 07:17:52 +0800

    chore : fix routing deploy problem

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import Login from "./components/login";
 import Post from "./components/post";
 import Home from "./components/home";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Routes>
         <Route path="/app" element={<App />} />
@@ -23,6 +23,6 @@ root.render(
         <Route path="/post" element={<Post />} />
         <Route path="/home" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
